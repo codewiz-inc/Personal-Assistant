28cf4f67b997032ce4255914fefdfb3ca142637a
codewiz-inc <35832831+codewiz-inc@users.noreply.github.com>
2018-01-28 15:57:01 +0530

    Update personal Assistant.py

--- personal Assistant.py
+++ personal Assistant.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python3
-# Requires PyAudio and PySpeech.
-
-
 import speech_recognition as sr
 from time import ctime
 import time
+import sys
 import webbrowser as wb
 import os
 import psutil
 import pyaudio
+import subprocess as sp
 from gtts import gTTS
+import urllib
+from urllib.request import urlopen
+from urllib.parse import quote
+import urllib.parse
+from bs4 import BeautifulSoup
+import fnmatch
 
 chrome_path='C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
@@ -37,19 +41,28 @@ def recordAudio():
         print("You said: " + data)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
-        speak(("Google Speech Recognition could not understand audio"))
+        #speak(("Google Speech Recognition could not understand audio"))
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
     return data
 
 
-def hs(data):
+def shaz(data):
     if "how are you" in data:
         speak("I am fine")
 
     if "what time is it" in data:
         speak(ctime())
+
+    if "open Notepad" in data:
+        programName = "notepad.exe"
+        sp.Popen([programName])
+
+    if "open calculator" in data:
+        programName = "C:\Windows\System32\calc.exe"
+        sp.Popen([programName])
+
 
     if "where is" in data:
         data = data.split(" ")
@@ -57,16 +70,8 @@ def hs(data):
         print(location)
         locstr= '+'.join(location)
         print(locstr)
-        speak("Hold on Boss, I will show you where it is.")
+        speak("Hold on, I will show you where it is.")
         wb.get(chrome_path).open("https://www.google.com/maps/place/" + locstr)
-
-    if "play video on" in data:
-        data = data.split(" ")
-        video = data[2:]
-        videostr = '+'.join(video)
-        print(videostr)
-        speak("Hold on ! Playing Video" )
-        wb.get(chrome_path).open("https://www.youtube.com/results?search_query=" + videostr)
 
 
     if "what is my battery percentage" in data:
@@ -83,18 +88,82 @@ def hs(data):
 
     if "search me about" in data:
         data = data.split(" ")
-        search = data[2:]
+        search = data[3:]
         print(search)
         searchstr = '+'.join(search)
         print(searchstr)
-        speak("searching,please wait"+searchstr)
+        speak("searching,please wait")
         wb.get(chrome_path).open("https://www.google.co.in/search?q="+ searchstr)
 
+    if "play video on" in data:
+        list=[]
+        data = data.split(" ")
+        video = data[3:]
+        print(video)
+        videostr = ' '.join(video)
+        print(videostr)
+        query = urllib.parse.quote(videostr)
+        url = "https://www.youtube.com/results?search_query=" + query
+        response = urllib.request.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html)
+        for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
+            list.append('https://www.youtube.com' + vid['href'])
+
+        print(list[0])
+        wb.get(chrome_path).open(list[0])
 
 
-# initialization
+    if "open Facebook" in data:
+        speak("please wait until your browser will open")
+        wb.get(chrome_path).open("https://www.facebook.com/")
+
+
+    if "open Twitter" in data:
+        speak("please wait until your browser will open")
+        wb.get(chrome_path).open("https://twitter.com/")
+
+
+
+    if "open LinkedIn" in data:
+        speak("please wait until your browser will open")
+        wb.get(chrome_path).open("https://www.linkedin.com/")
+
+
+
+    if "open Gmail" in data:
+        speak("please wait until your browser will open")
+        wb.get(chrome_path).open("https://mail.google.com/")
+
+    if "Run" in data or "run" in data:
+        data = data.split(" ")
+        openpro = data[1:]
+        print(openpro)
+        openstr=' '.join(openpro)
+        drives = ['C:\\', 'D:\\', 'E:\\', 'F:\\']
+        pattern = (openstr+".exe")
+        print(pattern)
+        filelist = []
+        with open("log.txt", "wb") as fs:
+            for rootPath in drives:
+                print("Now searching in: ", rootPath)
+                for root, dirs, files in os.walk(rootPath):
+                    for filename in fnmatch.filter(files, pattern):
+                        filepath = os.path.join(root, filename)
+                        print(filepath)
+                        filelist.append(filepath)
+                        print(filelist)
+                        result = os.path.join(root, filename)
+                        fs.write(bytearray((result + "\n"), 'utf8'))
+
+        print(filelist[0])
+        os.startfile(filelist[0])
+
+    if "exit" in data or "Thank you" in data:
+        sys.exit()
+
 time.sleep(1)
 speak("Hi dude, what's up?")
 while 1:
-    data = recordAudio()
-    hs(data)
+    data= recordAudio()
+    shaz(data)
